7463ddb3f7919497ad38d1fe73a5d8dff075c005
NateCWB <mattosnata99@gmail.com>
2024-06-24 19:32:52 -0300

    first commit

---
+++ backend-sales-system/config/database.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('sales_system', 'postgres', 'admin123', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+module.exports = sequelize;
